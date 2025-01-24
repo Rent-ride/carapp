763684041155fdcecc07dc50f954b478812d8015
Saqeef <rentridekhb@gmail.com>
2025-01-25 04:13:30 +0500

    "changed home animation"

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -12,7 +12,7 @@ function Card({title,color,description}) {
       },[])
   
   return (
-    <div className='border-2  flex flex-col items-center rounded-lg cursor-pointer hover:scale-[1.03] transition ease-in duration-150 h-full' data-aos="fade-up" data-aos-duration="700">
+    <div className='border-2  flex flex-col items-center rounded-lg cursor-pointer hover:scale-[1.03] transition ease-in duration-150 h-full' data-aos="fade-right" data-aos-duration="700">
         <div className={`${color ? "bg-[#fd8d14]" : "bg-[#0d274e]"} text-white inline-block p-3 rounded-[50%] absolute translate-y-[-50%] shadow-lg`}><Image src={"/logo.png"} alt='photo' width={50} height={50}/></div>
         <div className='bg-[#f2f4f9] w-full p-10 md:p-12'></div>        
         <div className='flex justify-center items-center flex-col mt-5'>
